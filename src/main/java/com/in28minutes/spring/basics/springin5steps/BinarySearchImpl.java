c085fb744dcb36bf1bff43eb895962069731c610
Robert Hamrick <Hamrick_Robert@bah.com>
2018-05-07 14:01:43 -0700

    Step 4

--- src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
@@ -1,11 +1,19 @@
 package com.in28minutes.spring.basics.springin5steps;
 
 public class BinarySearchImpl {
+	SortAlgorithm sortAlgorithm;
 	
+	public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
+		super();
+		this.sortAlgorithm = sortAlgorithm;
+		
+		// TODO Auto-generated constructor stub
+	}
+
 	public int binarySearch(int[] numbers, int numberToSearchFor) {
 	
-		BubbleSortAlgorithm bubbleSortAlgorithm = new BubbleSortAlgorithm();
-		int[] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
+		int[] sortedNumbers = sortAlgorithm.sort(numbers);
+		System.out.println(sortAlgorithm);
 		//Bubble Sort Algorithm
 		//Quick Sort Algorithm
 		
--- src/main/java/com/in28minutes/spring/basics/springin5steps/QuickSortAlgorithm.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/QuickSortAlgorithm.java
@@ -1,7 +1,7 @@
 package com.in28minutes.spring.basics.springin5steps;
 
-public class QuickSortAlgorithm {
-	public int[] quickSort(int[] numbers) {
+public class QuickSortAlgorithm implements SortAlgorithm{
+	public int[] sort(int[] numbers) {
 		return numbers;
 	}
 }
