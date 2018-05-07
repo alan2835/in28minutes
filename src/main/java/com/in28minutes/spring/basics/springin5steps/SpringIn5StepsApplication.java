c06f0d72dbdb0f32fbb3a58575b887f01ae489ec
Robert Hamrick <Hamrick_Robert@bah.com>
2018-05-07 14:18:42 -0700

    Step 5

--- src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -2,15 +2,22 @@ package com.in28minutes.spring.basics.springin5steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringIn5StepsApplication {
 
+	//What are the beans
+	//What are the dependencies of a bean.
+	//Where are the search paths. Automatically scans its own package/subpackages
+	
 	public static void main(String[] args) {
-		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
+		//BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
+		//Application Context
+		
+		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsApplication.class, args);
+		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
 		int result = binarySearch.binarySearch(new int[] {12,  4, 6 }, 3);
 		System.out.println(result);
-		
-		//SpringApplication.run(SpringIn5StepsApplication.class, args);
 	}
 }
