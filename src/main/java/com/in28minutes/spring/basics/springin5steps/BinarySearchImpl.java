0b50cfd9377eab68fdc7c26c1ce202f346487691
Robert Hamrick <Hamrick_Robert@bah.com>
2018-05-07 13:48:59 -0700

    Lecture 9

---
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
@@ -0,0 +1,14 @@
+package com.in28minutes.spring.basics.springin5steps;
+
+public class BinarySearchImpl {
+	
+	public int binarySearch(int[] numbers, int numberToSearchFor) {
+	
+		BubbleSortAlgorithm bubbleSortAlgorithm = new BubbleSortAlgorithm();
+		int[] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
+		//Bubble Sort Algorithm
+		//Quick Sort Algorithm
+		
+		return 3; //return index
+	}
+}
---
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/QuickSortAlgorithm.java
@@ -0,0 +1,7 @@
+package com.in28minutes.spring.basics.springin5steps;
+
+public class QuickSortAlgorithm {
+	public int[] quickSort(int[] numbers) {
+		return numbers;
+	}
+}
